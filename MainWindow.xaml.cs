4f2cd1cba2e7e2b1f0cf7fba1a39d1b538c1af48
JPKakaAG <Gjopka@yandex.ru>
2024-04-16 16:02:39 +0300

    gg

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,5 +20,76 @@ namespace prac18
         {
             InitializeComponent();
         }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadDBInDataGrid();
+        }
+        void LoadDBInDataGrid()
+        {
+            using (Devyatkinv11pr18Context _db = new Devyatkinv11pr18Context())
+            {
+                if (tbIndex.Text == "1")
+                {
+                    nTable.Content = "Таблица №1";
+                    btnLeft.Visibility = Visibility.Collapsed;
+                }
+                if (tbIndex.Text == "3")
+                {
+                    nTable.Content = "Таблица №3";
+                    btnRight.Visibility = Visibility.Collapsed;
+                }
+                if (tbIndex.Text == "2")
+                {
+                    nTable.Content = "Таблица №3";
+                    btnLeft.Visibility = Visibility.Visible;
+                    btnRight.Visibility = Visibility.Visible;
+                }
+                
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnView_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnInfo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRight_Click(object sender, RoutedEventArgs e)
+        {
+            int n = Convert.ToInt32(tbIndex.Text);
+            n += 1;
+            tbIndex.Text = n.ToString();
+        }
+
+        private void btnLeft_Click(object sender, RoutedEventArgs e)
+        {
+            int n = Convert.ToInt32(tbIndex.Text);
+            n -= 1;
+            tbIndex.Text = n.ToString();
+        }
     }
 }
